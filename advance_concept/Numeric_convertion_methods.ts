d1cf62cc8fe4b82c90ab88e67c7a44baa171c597
fanubalti786 <fanubalti786@gmail.com>
2024-05-15 00:22:41 +0500

    update

--- advance_concept/Numeric_convertion_methods.ts
+++ advance_concept/Numeric_convertion_methods.ts
@@ -6,11 +6,15 @@ let formattedNum: string = num1.toFixed(2);
 console.log(formattedNum); // Output: "123.46"
 
 
+
+
 // 1.toPrecision(): This method returns a string representing the number to a specified precision.
 
 let num2: number = 123.456789;
 let precisionNum: string = num2.toPrecision(4);
 console.log(precisionNum); // Output: "123.5"
+
+
 
 
 // 2.toString(): This method converts a number to a string.
@@ -20,11 +24,17 @@ let strNum: string = num3.toString();
 console.log(strNum); // Output: "123.45"
 
 
+
+
+
 // 3.toExponential(): This method returns a string representing the number in exponential notation
 
 let num4: number = 12345;
 let expNum: string = num4.toExponential(2);
 console.log(expNum); // Output: "1.23e+4"
+
+
+
 
 
 // 4.parseInt(): This function parses a string argument and returns an integer 
@@ -33,6 +43,11 @@ console.log(expNum); // Output: "1.23e+4"
 let str1: string = "10";
 let parsedNum: number = parseInt(str1);
 console.log(parsedNum); // Output: 10
+
+
+
+
+
 
 // 5.parseFloat(): This function parses a string argument and returns a floating point number.
 
